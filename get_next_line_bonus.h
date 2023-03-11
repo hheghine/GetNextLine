05b7317454e33ec0d16281eb50aec0168086d74f
cosmo-octopus <119530584+cosmo-octopus@users.noreply.github.com>
2023-03-11 13:19:24 +0400

    update 125/100

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbalasan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 17:45:58 by hbalasan          #+#    #+#             */
+/*   Updated: 2023/02/21 18:00:51 by hbalasan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+char	*ft_read_and_save(int fd, char *buffer);
+char	*ft_get_remainder(char *buffer);
+char	*ft_get_line(char *buffer);
+int		ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+
+#endif
